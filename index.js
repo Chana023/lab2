67137bacd91b9291a19028c9d199e4977e108b3c
Jesal Chana <jesalchana23@gmail.com>
2018-04-29 21:45:16 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let path = require('path');
+let express = require('express');
+let app = express();
+let mainRouter = require("./mainRoutes");
+
+app.use('/',mainRouter);
+
+//app.get('/', function (req,res) {
+//	res.send('hello world');
+//});
+
+//app.get('/about',function(req,res){
+//	res.sendFile(path.join(__dirname,'views','about.html'));
+//});
+
+app.listen(3000);
+console.log("Express server running on port 3000");
